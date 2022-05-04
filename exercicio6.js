ce550cec38115e7829f8e8f15f80a6d14438347e
Douglas-Lorenzi-Saldanha <103324394+Douglas-Lorenzi-Saldanha@users.noreply.github.com>
2022-05-04 12:03:57 -0300

    Create exercicio6.js

---
+++ exercicio6.js
@@ -0,0 +1,21 @@
+function startWithjava(start){
+
+  if (start.length < 4){
+    return false;
+  }
+
+  begin = (start.substring(0, 4));
+
+  if (begin == 'Java') {
+    return true;
+  }
+
+  else {
+    return false;
+  }
+}
+
+
+
+console.log(startWithjava("JavaScript"));
+console.log(startWithjava("C#"));
