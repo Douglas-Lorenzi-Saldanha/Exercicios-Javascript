2fab655592a2e0c23a85d50835a0e77f5dcc3aa9
Douglas-Lorenzi-Saldanha <103324394+Douglas-Lorenzi-Saldanha@users.noreply.github.com>
2022-05-13 18:55:54 -0300

    Update exercicio1.js

--- exercicio1.js
+++ exercicio1.js
@@ -1,11 +1,14 @@
 const data = new Date();
 const dia = data.getDay();
 let nomeDia;
+let turno;
 
 if(dia === 0){nomeDia = "Sunday";}else if(dia === 1){nomeDia = "Monday";}
 else if(dia === 2){nomeDia = "Tuesday";}else if(dia === 3){nomeDia = "Wednesday";}
 else if(dia === 4){nomeDia = "Thursday";}else if(dia === 5){nomeDia = "Friday";}
 else if(dia === 6){nomeDia = "Saturday";}
 
+if(data.getHours() > 12{turno = " PM : "} else{turno = " AM : "}
+
 console.log("Today is : " + nomeDia);
-console.log("Current time is : " + data.getHours() + " PM : " + data.getMinutes()  + " : " + data.getSeconds());
+console.log("Current time is : " + data.getHours() + turno + data.getMinutes()  + " : " + data.getSeconds());
